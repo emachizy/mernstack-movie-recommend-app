424aa5f617991dd43a76c5d388237b7fdb0f739c
emachizy <emachi2011@gmail.com>
2025-06-26 15:39:13 +0100

    refactor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// serviceWorkerRegistration.register();
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Movie Recommendation App",
+        short_name: "MovieApp",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#ff3131",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
